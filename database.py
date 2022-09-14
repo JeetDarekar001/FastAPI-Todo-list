00ac5165a7432ed66fd4fee4454211e4bf540fbb
Abhijit Darekar <darekarabhijit63@gmail.com>
2022-09-14 23:13:06 +0530

    Addedd connection to locally installed postgreSQL Database

--- database.py
+++ database.py
@@ -3,11 +3,17 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABSE_URL="sqlite:///./todos.db"
+#SQLALCHEMY_DATABSE_URL="sqlite:///./todos.db"
+
+#engine=create_engine(
+#   SQLALCHEMY_DATABSE_URL,
+#    connect_args={"check_same_thread":False}
+#)
+
+SQLALCHEMY_DATABSE_URL="postgresql://postgres:test1234@localhost/TodoApplicationDatabase"
 
 engine=create_engine(
-    SQLALCHEMY_DATABSE_URL,
-    connect_args={"check_same_thread":False}
+    SQLALCHEMY_DATABSE_URL
 )
 
 
