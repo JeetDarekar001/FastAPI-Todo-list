0af58978a5d8f67851494df1412ad4e06b60e637
Abhijit Darekar <darekarabhijit63@gmail.com>
2022-09-17 14:58:15 +0530

    Auth.py to perfrom login

--- routers/auth.py
+++ routers/auth.py
@@ -32,18 +32,9 @@ def get_db():
     finally:
         db.close()
 
-class CreateUser(BaseModel):
-    username : str
-    email : Optional[str]
-    first_name:str
-    last_name:str
-    password:str
-
 
 bcrpyt_context = CryptContext(schemes=["bcrypt"])
 
-def get_password_hash(password):
-    return bcrpyt_context.hash(password)
 
 def verifyPassword(plain_password, hash_password):
     return bcrpyt_context.verify(plain_password,hash_password)
@@ -83,23 +74,6 @@ async def get_current_user(token:str = Depends(oauth2_bearer)):
     except JWTError:
         raise get_user_exception()
 
-
-@router.post("/create/user")
-async def create_user(create_user:CreateUser,db:Session=Depends(get_db)):
-    create_user_model=models.Users()
-    create_user_model.email=create_user.email
-    create_user_model.username=create_user.username
-    create_user_model.firstname=create_user.first_name
-    create_user_model.lastname=create_user.last_name
-    create_user_model.hashedpassword=get_password_hash(create_user.password)
-    create_user_model.is_active=True
-    try:
-
-        db.add(create_user_model)
-        db.commit()
-        return {'Success':"user Created"}
-    except Exception as e :
-        raise HTTPException(status_code=500,detail=f'Username/Email Already Present={e}')
 
 # Signing using OAuth
 @router.post('/token')
